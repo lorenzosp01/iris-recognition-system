49a19e4ceb83a33d6fbec7aadf6f36e4ebc9c1d3
Alessio Pannozzo <flypilot.51@gmail.com>
2024-11-16 13:49:21 +0100

    Fix download_dataset

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,11 +1,16 @@
+import os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 def download_dataset(dataset_path='sondosaabed/casia-iris-thousand', path='./datasets'):
-    # Initialize Kaggle API
-    api = KaggleApi()
-    api.authenticate()
 
-    api.dataset_download_files(dataset_path, path=path, unzip=True)
+    dataset_name = dataset_path.split('/')[-1]
+    if not os.path.exists(path+dataset_name):
+        os.mkdir(path+dataset_name)
+        api = KaggleApi()
+        api.authenticate()
+
+        api.dataset_download_files(dataset_path, path=path, unzip=True)
+
 
 if __name__ == '__main__':
     download_dataset()
